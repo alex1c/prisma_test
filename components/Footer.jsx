98f99fa18426591901fdf31557154b15a513d655
alex1c <alex1c.spb@gmail.com>
2024-05-14 19:01:18 +0300

    14052024

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <><footer className="footer footer-center p-4 bg-base-300 text-base-content fixed 
+    inset-x-0 
+    bottom-0 
+">
+    <aside>
+      <p>Copyright © 2024 - All right reserved by Forest-Music Ltd</p>
+    </aside>
+  </footer></>
+  )
+}
+
+export default Footer
